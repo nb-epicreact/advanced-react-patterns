c1a5a4fa71bc55064c54051d958888ddc2f5dae7
Nate Boyette <nateboyette@icloud.com>
2021-08-15 01:08:56 -0700

    ex05_ec02

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -9,12 +9,17 @@ const callAll =
   (...args) =>
     fns.forEach(fn => fn?.(...args))
 
+const actionTypes = {
+  TOGGLE: 'toggle',
+  RESET: 'reset',
+}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.TOGGLE: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.RESET: {
       return initialState
     }
     default: {
@@ -29,8 +34,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.TOGGLE})
+  const reset = () => dispatch({type: actionTypes.RESET, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -55,15 +60,15 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
     getResetterProps,
   }
 }
-// export {useToggle, toggleReducer}
+// export {useToggle, toggleReducer, actionTypes}
 
-// import {useToggle, toggleReducer} from './use-toggle'
+// import {useToggle, toggleReducer, actionTypes} from './use-toggle'
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && clickedTooMuch) {
+    if (action.type === actionTypes.TOGGLE && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
